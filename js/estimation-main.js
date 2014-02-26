7162ba5ae95dc3011a3330c448fb077e34b45804
jbphet <john.blanco@colorado.edu>
2014-02-25 18:42:47 -0800

    Started adding basic project structure, not done yet.

---
+++ js/estimation-main.js
@@ -0,0 +1,47 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Main entry point for the 'Estimation' sim.
+ *
+ * @author John Blanco
+ */
+define( function( require ) {
+  'use strict';
+
+  // imports
+  var EstimationScreen = require( 'ESTIMATION/view/EstimationScreen' );
+  var Screen = require( 'JOIST/Screen' );
+  var Sim = require( 'JOIST/Sim' );
+  var SimLauncher = require( 'JOIST/SimLauncher' );
+
+  // strings
+  var simTitle = require( 'string!ESTIMATION/estimation.name' );
+
+  var simOptions = {
+    credits: {
+      leadDesign: 'Bryce Gruneich',
+      softwareDevelopment: 'John Blanco',
+      designTeam: ', Mike Dubson, Karina Hensberry, Ariel Paul, Kathy Perkins'
+    }
+  };
+
+  // Appending '?dev' to the URL will enable developer-only features.
+  if ( window.phetcommon.getQueryParameter( 'dev' ) ) {
+    simOptions = _.extend( {
+      // add dev-specific options here
+      showHomeScreen: false,
+      screenIndex: 0
+    }, simOptions );
+  }
+
+  SimLauncher.launch( function() {
+
+    //Create and start the sim
+    new Sim( simTitle, [
+      new Screen( tugOfWarString, new Image( TugIcon ),
+        function() {return new TugOfWarModel();},
+        function( model ) {return new TugOfWarView( model );}
+      )
+    ], simOptions ).start();
+  } );
+} );
---
+++ js/view/EstimationScreen.js
@@ -0,0 +1,18 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+define( function( require ) {
+  'use strict';
+
+  // Imports
+  var inherit = require( 'PHET_CORE/inherit' );
+  var ScreenView = require( 'JOIST/ScreenView' );
+
+
+  function EstimationScreen() {
+    ScreenView.call( this );
+  }
+
+  return inherit( ScreenView, EstimationScreen, {
+    //TODO prototypes
+  } );
+} );
