a552f2c06ed9de58cdd15d783f77c4c9485ee2d4
jbphet <john.blanco@colorado.edu>
2014-03-05 11:39:11 -0800

    Added initial set of properties to explore model, added some initial documentation.

---
+++ js/common/EstimationConstants.js
@@ -0,0 +1,11 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+define( function( require ) {
+  'use strict';
+
+  return {
+    RANGE_1_TO_10: { min: 1, max: 10 },
+    RANGE_10_TO_100: { min: 10, max: 100 },
+    RANGE_100_TO_1000: { min: 100, max: 1000 }
+  };
+} );
--- js/explore/model/ExploreModel.js
+++ js/explore/model/ExploreModel.js
@@ -3,11 +3,35 @@
 define( function( require ) {
   'use strict';
 
+  // Imports
   var Property = require( 'AXON/Property' );
+  var EstimationConstants = require( 'ESTIMATION/common/EstimationConstants' );
 
+  // Constants
+  var RANGE_1_TO_10 = { min: 1, max: 10 };
+
+  /**
+   * @constructor
+   */
   function EstimationModel() {
-    this.soundEnabled = new Property( true );
+
+    // Externally visible properties.
+    this.soundEnabled = new Property( true ); // TODO: Is there really any sound for the 'Explore' screen?
+    this.estimationMode = new Property( 'cubes' ); // Valid values are 'lines', 'rectangles', 'cubes', and 'cylinders'.
+    this.estimationRange = new Property( EstimationConstants.RANGE_1_TO_10 );
+    this.quantity = new Property( 1 ); // Quantity of estimated objects
+    this.fillType = new Property( 'discrete' ); // Valid values are 'discrete' or 'continuous'.
   }
+
+  EstimationModel.prototype = {
+    reset: function() {
+      this.soundEnabled.reset();
+      this.estimationMode.reset();
+      this.estimationRange.reset();
+      this.quantity.reset();
+      this.fillType.reset();
+    }
+  };
 
   return EstimationModel;
 } );
