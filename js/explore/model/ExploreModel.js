674c49203d8756d04cc5251a19b94425657bae78
jbphet <john.blanco@colorado.edu>
2014-03-05 13:54:25 -0800

    Added icons and control panel for specifying estimation mode.

--- js/explore/model/ExploreModel.js
+++ js/explore/model/ExploreModel.js
@@ -7,31 +7,28 @@ define( function( require ) {
   var Property = require( 'AXON/Property' );
   var EstimationConstants = require( 'ESTIMATION/common/EstimationConstants' );
 
-  // Constants
-  var RANGE_1_TO_10 = { min: 1, max: 10 };
-
   /**
    * @constructor
    */
-  function EstimationModel() {
+  function ExploreModel() {
 
     // Externally visible properties.
-    this.soundEnabled = new Property( true ); // TODO: Is there really any sound for the 'Explore' screen?
-    this.estimationMode = new Property( 'cubes' ); // Valid values are 'lines', 'rectangles', 'cubes', and 'cylinders'.
-    this.estimationRange = new Property( EstimationConstants.RANGE_1_TO_10 );
-    this.quantity = new Property( 1 ); // Quantity of estimated objects
-    this.fillType = new Property( 'discrete' ); // Valid values are 'discrete' or 'continuous'.
+    this.soundEnabledProperty = new Property( true ); // TODO: Is there really any sound for the 'Explore' screen?
+    this.estimationModeProperty = new Property( 'cubes' ); // Valid values are 'lines', 'rectangles', 'cubes', and 'cylinders'.
+    this.estimationRangeProperty = new Property( EstimationConstants.RANGE_1_TO_10 );
+    this.quantityProperty = new Property( 1 ); // Quantity of estimated objects
+    this.fillTypeProperty = new Property( 'discrete' ); // Valid values are 'discrete' or 'continuous'.
   }
 
-  EstimationModel.prototype = {
+  ExploreModel.prototype = {
     reset: function() {
-      this.soundEnabled.reset();
-      this.estimationMode.reset();
-      this.estimationRange.reset();
-      this.quantity.reset();
-      this.fillType.reset();
+      this.soundEnabledProperty.reset();
+      this.estimationModeProperty.reset();
+      this.estimationRangeProperty.reset();
+      this.quantityProperty.reset();
+      this.fillTypeProperty.reset();
     }
   };
 
-  return EstimationModel;
+  return ExploreModel;
 } );
--- js/explore/view/ExploreScreen.js
+++ js/explore/view/ExploreScreen.js
@@ -8,20 +8,27 @@ define( function( require ) {
 
   // Imports
   var Color = require( 'SCENERY/util/Color' );
+  var cubesIconImage = require( 'image!ESTIMATION/cubes-icon.png' );
+  var cylindersIconImage = require( 'image!ESTIMATION/cylinders-icon.png' );
+  var Image = require( 'SCENERY/nodes/Image' );
+  var InOutRadioButton = require( 'SUN/InOutRadioButton' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var linesIconImage = require( 'image!ESTIMATION/lines-icon.png' );
+  var newObjectString = require( 'string!ESTIMATION/newObject' );
+  var Panel = require( 'SUN/Panel' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var PushButton = require( 'SUN/PushButton' );
   var RectanglePushButton = require( 'SUN/RectanglePushButton' );
   var ResetAllButton = require( 'SCENERY_PHET/ResetAllButton' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var SoundToggleButton = require( 'SCENERY_PHET/SoundToggleButton' );
+  var squaresIconImage = require( 'image!ESTIMATION/squares-icon.png' );
   var Text = require( 'SCENERY/nodes/Text' );
-
-  var newObjectString = require( 'string!ESTIMATION/newObject' );
-
+  var VBox = require( 'SCENERY/nodes/VBox' );
 
   // Constants
   var EDGE_INSET = 10;
+  var BUTTON_IMAGE_WIDTH = 50; // In screen coords, which are roughly pixels
 
   /**
    * @param model
@@ -31,7 +38,7 @@ define( function( require ) {
     ScreenView.call( this );
     var thisScreen = this;
 
-    // Add the various selectors and buttons for choosing which objects to explore
+    // Add the various selectors and buttons for choosing which objects to explore.
     var newObjectButton = new RectanglePushButton( new Text( newObjectString, { font: new PhetFont( 20 ) } ),
       {
         rectangleFillUp: new Color( 255, 252, 127 ),
@@ -41,13 +48,32 @@ define( function( require ) {
     );
     this.addChild( newObjectButton );
 
-    // Add the general control buttons
+    // Create and add the panel for controlling the estimation type (i.e. cubes, lines, etc).
+    var inOutButtonOptions = { xMargin: 3, yMargin: 3 };
+
+    var modeControlPanel = new Panel( new VBox(
+      {
+        children: [
+          new InOutRadioButton( model.estimationModeProperty, 'lines', this.createImageOfGivenWidth( linesIconImage, BUTTON_IMAGE_WIDTH ), inOutButtonOptions ),
+          new InOutRadioButton( model.estimationModeProperty, 'squares', this.createImageOfGivenWidth( squaresIconImage, BUTTON_IMAGE_WIDTH ), inOutButtonOptions ),
+          new InOutRadioButton( model.estimationModeProperty, 'cubes', this.createImageOfGivenWidth( cubesIconImage, BUTTON_IMAGE_WIDTH ), inOutButtonOptions ),
+          new InOutRadioButton( model.estimationModeProperty, 'cylinders', this.createImageOfGivenWidth( cylindersIconImage, BUTTON_IMAGE_WIDTH ), inOutButtonOptions )
+        ],
+        spacing: 10
+      } ),
+      { fill: 'rgb( 0, 171, 51 )', stroke: null, yMargin: 10 } );
+
+    this.addChild( modeControlPanel );
+
+    // Add the general control buttons.
     var resetAllButton = new ResetAllButton( function() { thisScreen.reset(); }, { scale: 0.75 } );
     this.addChild( resetAllButton );
-    var soundToggleButton = new SoundToggleButton( model.soundEnabled );
+    var soundToggleButton = new SoundToggleButton( model.soundEnabledProperty );
     this.addChild( soundToggleButton );
 
     // Layout
+    modeControlPanel.top = EDGE_INSET;
+    modeControlPanel.left = EDGE_INSET;
     newObjectButton.top = EDGE_INSET;
     newObjectButton.left = 100;
     resetAllButton.right = this.layoutBounds.width - EDGE_INSET;
@@ -59,6 +85,12 @@ define( function( require ) {
   return inherit( ScreenView, ExploreScreen, {
     reset: function() {
       console.log( 'Reset not yet implemented' );
+    },
+
+    createImageOfGivenWidth: function( imageSource, width ) {
+      var imageNode = new Image( imageSource );
+      imageNode.scale( width / imageNode.width );
+      return imageNode;
     }
   } );
 } );
