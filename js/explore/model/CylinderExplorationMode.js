363d3c03e154384b7e6ab24424935e4d988f997b
samreid <reids@colorado.edu>
2017-09-21 10:19:48 -0600

    Updated copyright dates, see https://github.com/phetsims/chipper/issues/403

--- js/explore/model/CylinderExplorationMode.js
+++ js/explore/model/CylinderExplorationMode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2015, University of Colorado Boulder
+// Copyright 2014-2017, University of Colorado Boulder
 
 /**
  * Definition of the 'cylinder exploration mode' for the exploration model.
