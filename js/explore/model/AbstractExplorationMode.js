a6f5b913f0373f0270101d9cd90454b9965bed63
phet-dev <phetadmin@gmail.com>
2019-09-20 03:11:50 -0600

    update copyright dates from daily grunt work

--- js/explore/model/AbstractExplorationMode.js
+++ js/explore/model/AbstractExplorationMode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * Base class for the various modes that the user can select in the "Explore"
