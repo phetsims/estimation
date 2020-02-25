325c75f376c5446f3ab534a7e9767165acb98782
phet-dev <phetadmin@gmail.com>
2020-02-25 03:19:43 -0700

    update copyright dates from daily grunt work

--- js/common/view/CubeBackView.js
+++ js/common/view/CubeBackView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * View representation of the back portion of a cube, which is to say the
--- js/common/view/RectangleView.js
+++ js/common/view/RectangleView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * View representation of a rectangle used within the Estimation simulation.
