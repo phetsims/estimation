f79bbebe5a5eee8f24c32fba0fc1467976d70e0f
samreid <reids@colorado.edu>
2020-04-24 10:43:18 -0600

    grunt modulify to use SimLauncher locks, see https://github.com/phetsims/joist/issues/621

--- images/lines-icon_png.js
+++ images/lines-icon_png.js
@@ -1,5 +1,7 @@
 /* eslint-disable */
-const img = new Image();
-window.phetImages.push( img );
-img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAAALCAYAAADP9otxAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAHhJREFUeNpijIj6tZ6BgSGAgUjAxMT69t3bMwU7tpsuYRgGgAmIHUjUwwPElgzDBLAAcSIQGwLxfyID7CMQr2EYBaNgFIyCUTD0ASMuCVFOFrZXuVohv3781QJy/wzXAGDBI8cPxEuHewpgwiP3C4gvD3P/fwAIMABW6hZHARBfrwAAAABJRU5ErkJggg==';
-export default img;
+import SimLauncher from '../../joist/js/SimLauncher.js';
+const image = new Image();
+const unlock = SimLauncher.createLock( image );
+image.onload = unlock;
+image.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAAALCAYAAADP9otxAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAHhJREFUeNpijIj6tZ6BgSGAgUjAxMT69t3bMwU7tpsuYRgGgAmIHUjUwwPElgzDBLAAcSIQGwLxfyID7CMQr2EYBaNgFIyCUTD0ASMuCVFOFrZXuVohv3781QJy/wzXAGDBI8cPxEuHewpgwiP3C4gvD3P/fwAIMABW6hZHARBfrwAAAABJRU5ErkJggg==';
+export default image;
--- images/squares-icon_png.js
+++ images/squares-icon_png.js
@@ -1,5 +1,7 @@
 /* eslint-disable */
-const img = new Image();
-window.phetImages.push( img );
-img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADcAAAAkCAYAAAAtmaJzAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAH1JREFUeNrs1sEJhEAMBdBRxGZswu1CLMIatBmxi7EU29g97VTgXEQceR9CLrk8CCRVuDDfqYup9eEhqcOLAwcHBwcHBwcHd2uaYfzlHt1jW9ujSFyqmJlZUs3WEg4ODg6u0Dv3yR3xYnHp+9itJRwcHBwcHBwcHNx5/gIMALO+DNb1Gsp2AAAAAElFTkSuQmCC';
-export default img;
+import SimLauncher from '../../joist/js/SimLauncher.js';
+const image = new Image();
+const unlock = SimLauncher.createLock( image );
+image.onload = unlock;
+image.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADcAAAAkCAYAAAAtmaJzAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAH1JREFUeNrs1sEJhEAMBdBRxGZswu1CLMIatBmxi7EU29g97VTgXEQceR9CLrk8CCRVuDDfqYup9eEhqcOLAwcHBwcHBwcHd2uaYfzlHt1jW9ujSFyqmJlZUs3WEg4ODg6u0Dv3yR3xYnHp+9itJRwcHBwcHBwcHNx5/gIMALO+DNb1Gsp2AAAAAElFTkSuQmCC';
+export default image;
