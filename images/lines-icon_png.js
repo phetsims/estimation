01dde1a5d8483fb8a58c4cf04501dc1fe386f489
zepumph <michael.kauzmann@colorado.edu>
2020-05-07 12:37:26 -0800

    Convert SimLauncher to es6 class, rename to simLauncher.js, https://github.com/phetsims/joist/issues/630

--- images/lines-icon_png.js
+++ images/lines-icon_png.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
-import SimLauncher from '../../joist/js/SimLauncher.js';
+import simLauncher from '../../joist/js/simLauncher.js';
 const image = new Image();
-const unlock = SimLauncher.createLock( image );
+const unlock = simLauncher.createLock( image );
 image.onload = unlock;
 image.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAAALCAYAAADP9otxAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAHhJREFUeNpijIj6tZ6BgSGAgUjAxMT69t3bMwU7tpsuYRgGgAmIHUjUwwPElgzDBLAAcSIQGwLxfyID7CMQr2EYBaNgFIyCUTD0ASMuCVFOFrZXuVohv3781QJy/wzXAGDBI8cPxEuHewpgwiP3C4gvD3P/fwAIMABW6hZHARBfrwAAAABJRU5ErkJggg==';
 export default image;
--- images/squares-icon_png.js
+++ images/squares-icon_png.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
-import SimLauncher from '../../joist/js/SimLauncher.js';
+import simLauncher from '../../joist/js/simLauncher.js';
 const image = new Image();
-const unlock = SimLauncher.createLock( image );
+const unlock = simLauncher.createLock( image );
 image.onload = unlock;
 image.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADcAAAAkCAYAAAAtmaJzAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAH1JREFUeNrs1sEJhEAMBdBRxGZswu1CLMIatBmxi7EU29g97VTgXEQceR9CLrk8CCRVuDDfqYup9eEhqcOLAwcHBwcHBwcHd2uaYfzlHt1jW9ujSFyqmJlZUs3WEg4ODg6u0Dv3yR3xYnHp+9itJRwcHBwcHBwcHNx5/gIMALO+DNb1Gsp2AAAAAElFTkSuQmCC';
 export default image;
