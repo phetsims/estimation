f99227a7e328730c35543018a497b0c229e1b29f
zepumph <michael.kauzmann@colorado.edu>
2021-01-22 13:10:25 -0900

    use asyncLoader instead of simLauncher, plus grunt update on accident, https://github.com/phetsims/joist/issues/687

--- images/squares-icon_png.js
+++ images/squares-icon_png.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
-import simLauncher from '../../joist/js/simLauncher.js';
+import asyncLoader from '../../phet-core/js/asyncLoader.js';
 
 const image = new Image();
-const unlock = simLauncher.createLock( image );
+const unlock = asyncLoader.createLock( image );
 image.onload = unlock;
 image.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADcAAAAkCAYAAAAtmaJzAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAH1JREFUeNrs1sEJhEAMBdBRxGZswu1CLMIatBmxi7EU29g97VTgXEQceR9CLrk8CCRVuDDfqYup9eEhqcOLAwcHBwcHBwcHd2uaYfzlHt1jW9ujSFyqmJlZUs3WEg4ODg6u0Dv3yR3xYnHp+9itJRwcHBwcHBwcHNx5/gIMALO+DNb1Gsp2AAAAAElFTkSuQmCC';
 export default image;
