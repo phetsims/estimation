e4341f10d1eecd60ccde52626a4baed211743fc9
phet-dev <phetadmin@gmail.com>
2021-05-25 15:44:54 -0600

    update copyright dates from daily grunt work

--- js/explore/model/LineExplorationMode.js
+++ js/explore/model/LineExplorationMode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2021, University of Colorado Boulder
 
 /**
  * Definition of the 'line exploration mode' for the exploration model.
