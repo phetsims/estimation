6b286b928c79cfd0b371c61fb6e6fd69465603a7
phet-dev <phetadmin@gmail.com>
2021-11-26 03:15:53 -0700

    update copyright dates from daily grunt work

--- js/game/view/EstimationGameScreenView.js
+++ js/game/view/EstimationGameScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2021, University of Colorado Boulder
 
 import ScreenView from '../../../../joist/js/ScreenView.js';
 import { Image } from '../../../../scenery/js/imports.js';
