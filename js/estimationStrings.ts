4e1610debe00613846ab2a60b259e28750627d24
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:41 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/estimationStrings.ts
+++ js/estimationStrings.ts
@@ -5,15 +5,20 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import estimation from './estimation.js';
 
 type StringsType = {
   'estimation': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'explore': string;
+  'exploreProperty': TReadOnlyProperty<string>;
   'game': string;
+  'gameProperty': TReadOnlyProperty<string>;
   'newObject': string;
+  'newObjectProperty': TReadOnlyProperty<string>;
 };
 
 const estimationStrings = getStringModule( 'ESTIMATION' ) as StringsType;
