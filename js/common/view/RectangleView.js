633731c8318e072015f0cb1b7e09920606da44f8
phet-dev <phetadmin@gmail.com>
2024-01-19 03:12:55 -0700

    update copyright dates from daily grunt work

--- js/common/view/RectangleView.js
+++ js/common/view/RectangleView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * View representation of a rectangle used within the Estimation simulation.
